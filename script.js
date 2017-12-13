5499d6c2cdb1c4cd9fbdc754176c3d2c0ec404c8
Matt <[mailprogramisty@gmail.com]>
2017-12-13 18:03:34 +0100

    email update

--- script.js
+++ script.js
@@ -44,43 +44,27 @@ $(document).on('scroll', function(){
        }
    });
 });
+// Sending mail
 var $contactForm = $('.contact-form');
+var $submitBtn = $('#submit');
 $contactForm.submit(function(e) {
     e.preventDefault();
     $.ajax({
         url: "https://formspree.io/mateuszdanieluk@gmail.com",
         method: "POST",
         data: $(this).serialize(),
-		dataType: 'json',
+        dataType: 'json',
 		beforeSend: function() {
-            $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+            $submitBtn.empty();
+            $submitBtn.append('<div class="alert alert-loading">Sending message…</div>');
         },
         success: function(data) {
-			$contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+			$submitBtn.find('.alert-loading').hide();
+            $submitBtn.append('<div class="alert alert-success">Message sent!</div>');
         },
         error: function(err) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-		}
+            $submitBtn.find('.alert-loading').hide();
+			$submitBtn.append('<div class="alert alert-error">Ops, try again</div>');
+        },
 })
 })
-/*
-$('.contact-form').submit(function(e) {
-    var name = $('#name');
-    var email = $('#email');
-    var message = $('#msg');
-    if(!name.value || !email.value || !message.value) {
-        alertify.error('Sooo... U wanna contact Me or not?')
-    } else {
-        $.ajax({
-            url: "https://formspree.io/mateuszdanieluk@gmail.com", 
-            method: "POST",
-            data: $(this).serialize(),
-            dataType: "json"
-        });
-        e.preventDefault();
-        $(this).get(0).reset();
-        alertify.success('Message sent');
-    }
-})*/
